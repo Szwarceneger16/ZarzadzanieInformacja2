2dfdfe42fc91a1442c501b9d21a9e6e1d808ea16
Grzegorz Szwarc <sg44522@zut.edu.pl>
2020-11-12 15:59:40 +0100

    added empty projects for lab6

---
+++ 06/main.php
@@ -0,0 +1,9 @@
+<?php
+
+$connectionParams = array(
+    'dbname' => 'nowa_pusta_php',
+    'user' => 'root',
+    'password' => 'jajeczko',
+    'host' => 'localhost',
+    'driver' => 'pdo_mysql',
+);
